192221007d3bc41063a1fa3f2f09ee8b339518b6
amanuelcm27 <amanuelfirew27@gmail.com>
2025-12-26 18:53:22 +0300

    secret moved to env

--- lighthouse/settings.py
+++ lighthouse/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zt0u6w0r$&ks*hprpmxzd==q6_*r94-p!t3sciptkr8w_wu=0='
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
